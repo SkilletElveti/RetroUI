6f5d4867a04b9e63f1d60a196bab53e1d203103f
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-12-02 09:34:14 -0800

    - Added Example SwiftUI App

---
+++ RetroUIExample/RetroUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroUIExampleApp.swift
+//  RetroUIExample
+//
+//  Created by Shubham Kamdi on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RetroUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
