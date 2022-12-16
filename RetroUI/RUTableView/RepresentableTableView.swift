cd94fcc0c4b1840212462286374e103d21201250
Shubham Kamdi <shubhamvkamdi@gmail.com>
2022-12-15 21:27:11 -0800

    - Initializer added to UITableView

--- RetroUI/RUTableView/RepresentableTableView.swift
+++ RetroUI/RUTableView/RepresentableTableView.swift
@@ -8,5 +8,22 @@
 import Foundation
 import UIKit
 class RepresentableTableView: UITableView {
+    init() {
+        super.init(frame: CGRectZero, style: UITableView.Style.plain)
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
     
+    init(frame: CGRect) {
+        super.init(frame: frame, style: UITableView.Style.plain)
+       
+    }
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        
+    }
 }
